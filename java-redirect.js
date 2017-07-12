f9e224fb57b67bbf8a639aa5c3b3e2a70da120f6
RoiEX <RoiEXLab@users.noreply.github.com>
2017-07-12 13:03:55 +0200

    Support http

--- java-redirect.js
+++ java-redirect.js
@@ -1,4 +1,4 @@
-var newLoc = String(window.location).replace(/https\:\/\/docs\.oracle\.com\/javase\/[678]/g, "https://docs.oracle.com/javase/8");
+var newLoc = String(window.location).replace(/https?\:\/\/docs\.oracle\.com\/javase\/[678]/g, "https://docs.oracle.com/javase/8");
 if(newLoc != String(window.location)){
 	window.location = newLoc;
 }
