0f9329dd05441643b087ae2be141c97a255595fb
RoiEX <RoiEXLab@users.noreply.github.com>
2017-08-25 18:55:38 +0200

    Rework Plugin to redirect before requesting

--- java-redirect.js
+++ java-redirect.js
@@ -1,4 +1,14 @@
-var newLoc = String(window.location).replace(/https?\:\/\/docs\.oracle\.com\/javase\/[678]/g, "https://docs.oracle.com/javase/8");
-if(newLoc != String(window.location)){
-	window.location = newLoc;
+browser.webRequest.onBeforeRequest.addListener(redirectJava, {urls: ["*://docs.oracle.com/javase/*/docs/*"]}, ["blocking"]);
+
+function redirectJava(details){
+	const redirectionTarget = details.url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, "https://docs.oracle.com/javase/8");
+	if(redirectionTarget !== details.url){
+		const request = new XMLHttpRequest();
+		request.open('HEAD', redirectionTarget, false);
+		request.send(null);
+		if(request.status === 200){
+		  return {redirectUrl: redirectionTarget};
+		}
+	}
+	return {};
 }
