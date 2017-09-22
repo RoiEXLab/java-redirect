162372f64412887445dda1d02d7ee858a36224a4
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-09-22 12:40:13 +0200

    Update to Java 9

--- java-redirect.js
+++ java-redirect.js
@@ -1,7 +1,7 @@
 browser.webRequest.onBeforeRequest.addListener(redirectJava, {urls: ["*://docs.oracle.com/javase/*/docs/*"]}, ["blocking"]);
 
 function redirectJava(details){
-	const redirectionTarget = details.url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, "https://docs.oracle.com/javase/8");
+	const redirectionTarget = details.url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[6789]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, "https://docs.oracle.com/javase/9");
 	if(redirectionTarget !== details.url){
 		const request = new XMLHttpRequest();
 		request.open('HEAD', redirectionTarget, false);
