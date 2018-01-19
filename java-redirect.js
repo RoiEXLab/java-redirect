3e9365f0b085e5de62666dcec0b89d7c6f213b9c
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-01-19 18:19:41 +0100

    Add settings for redirection version
    Closes  #1

--- java-redirect.js
+++ java-redirect.js
@@ -1,14 +1,23 @@
-browser.webRequest.onBeforeRequest.addListener(redirectJava, {urls: ["*://docs.oracle.com/*"]}, ["blocking"]);
-
-function redirectJava(details){
-	const redirectionTarget = details.url.replace(/^https?\:\/\/docs\.oracle\.com\/(javafx\/2|javafx\/(?:[6789]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/(docs|javafx))/g, "https://docs.oracle.com/javase/9/docs");
-	if(redirectionTarget !== details.url){
-		const request = new XMLHttpRequest();
-		request.open('HEAD', redirectionTarget, false);
-		request.send(null);
-		if(request.status === 200 && request.responseURL === redirectionTarget){
-		  return {redirectUrl: redirectionTarget};
+browser.storage.sync.get().then(result => {
+	const replaceUrl = function(setting){
+		switch(setting){
+			case "8":
+				return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, "https://docs.oracle.com/javase/8");
+			default:
+				return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/(javafx\/2|(javafx|javase)\/(?:[6789]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/(docs|javafx))/g, "https://docs.oracle.com/javase/9/docs");
 		}
-	}
-	return {};
-}
+	}(result['java-version']);
+
+	browser.webRequest.onBeforeRequest.addListener(details => {
+		const redirectionTarget = replaceUrl(details.url);
+		if(redirectionTarget !== details.url){
+			const request = new XMLHttpRequest();
+			request.open('HEAD', redirectionTarget, false);
+			request.send(null);
+			if(request.status === 200 && request.responseURL === redirectionTarget){
+			  return {redirectUrl: redirectionTarget};
+			}
+		}
+		return {};
+	}, {urls: ["*://docs.oracle.com/*"]}, ["blocking"]);
+});
---
+++ settings.js
@@ -0,0 +1,8 @@
+const settings = document.querySelector('#settings');
+settings.addEventListener('change', () => browser.storage.sync.set({
+  'java-version': settings.value
+}));
+
+document.addEventListener('DOMContentLoaded',
+  () => browser.storage.sync.get()
+    .then(result => settings.value = result['java-version'] || "default"));
