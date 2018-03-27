58ca161a1cc88ead7f38c4371d50a43ca20c4070
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-03-27 19:32:22 +0200

    Update to Java 10

--- java-redirect.js
+++ java-redirect.js
@@ -1,12 +1,14 @@
 browser.storage.sync.get().then(result => {
-	const replaceUrl = function(setting){
-		switch(setting){
+	const replaceUrl = function(){
+		switch(result['java-version']){
 			case '8':
 				return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, 'https://docs.oracle.com/javase/8');
-			default:
+			case '9':
 				return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/(javafx\/2|(javafx|javase)\/(?:[6789]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/(docs|javafx))/g, 'https://docs.oracle.com/javase/9/docs');
+			default:
+				return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/(javafx\/2|(javafx|javase)\/(?:[6789]|10|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/(docs|javafx))/g, 'https://docs.oracle.com/javase/10/docs');
 		}
-	}(result['java-version']);
+	}();
 
 	browser.webRequest.onBeforeRequest.addListener(details => {
 		const redirectionTarget = replaceUrl(details.url);
