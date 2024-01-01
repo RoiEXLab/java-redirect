92b53b6964bc4615359d5ff0d4c6eb16ba41d112
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2024-01-01 17:33:55 +0100

    Add back files for javafx compatibility

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java-redirect
 A Firefox Addon which redirects you to the latest javadocs
+
+### Test URLs
+https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/Dialog.html
+https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpClient.html
--- java-redirect.js
+++ java-redirect.js
@@ -11,7 +11,9 @@ const computeModuleMap = async (version) => {
 
 const getReplaceAlgorithm = async version => {
   const versionInt = parseInt(version, 10) || 21;
-  const moduleMap = versionInt >= 11 ? await computeModuleMap(versionInt) : null;
+  const [moduleMap, javaFxModuleMap] = versionInt >= 11
+    ? await Promise.all([computeModuleMap(versionInt), computeModuleMap(10)])
+    : null;
 	switch (versionInt) {
 		case 8:
 			return url => url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))/g, 'https://docs.oracle.com/javase/8');
@@ -22,77 +24,77 @@ const getReplaceAlgorithm = async version => {
 		case 11:
 		  return url => {
 				if (url.includes('javafx')) {
-					return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|javase\/(?:[89]|10)\/javafx)\/api\/([a-z/]+)\//g, (_, g1) => `https://openjfx.io/javadoc/11/${moduleMap[g1.replace(/\//g, '.')]}/${g1}/`);
+					return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|javase\/(?:[89]|10)\/javafx)\/api\/([a-z/]+)\//g, (_, g1) => `https://openjfx.io/javadoc/11/${javaFxModuleMap[g1.replace(/\//g, '.')]}/${g1}/`);
 				}
 				return url.replace(/^https?\:\/\/docs\.oracle\.com\/javase\/(?:[6789]|10|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/([a-z/]+)\//g, (_, g1) => `https://docs.oracle.com/en/java/javase/11/docs/api/${moduleMap[g1.replace(/\//g, '.')]}/${g1}/`);
 			};
     case 12:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|10|11)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/12/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|10|11)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/12/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|10|11|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/12/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 13:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/13/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/13/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[012]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/13/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 14:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[0123])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/14/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[0123])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/14/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[0123]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/14/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 15:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[01234])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/15/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[01234])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/15/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[01234]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/15/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 16:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012345])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/16/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012345])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/16/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[012345]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/16/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 17:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[0123456])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/17/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[0123456])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/17/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[0123456]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/17/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 18:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[01234567])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/18/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[01234567])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/18/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[01234567]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/18/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 19:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012345678])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/19/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1[012345678])\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/19/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1[012345678]|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/19/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 20:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1\d)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/20/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1\d)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/20/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1\d|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/20/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
     case 21:
       return url => {
         if (url.includes('javafx')) {
-          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1\d|20)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/21/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
+          return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:javafx\/2|(?:en\/java\/)?javase\/(?:[89]|1\d|20)\/javafx)\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://openjfx.io/javadoc/21/${g1 || javaFxModuleMap[g2.replace(/\//g, '.')]}/${g2}/`);
         }
         return url.replace(/^https?\:\/\/docs\.oracle\.com\/(?:en\/java\/)?javase\/(?:[6789]|1\d|20|(?:1\.(?:(?:5\.0)|(?:4\.2)|[3])))\/docs\/api\/(?:((?:java|jdk)\.(?:\w|\.)+)\/)?([a-z/]+)\//g, (_, g1, g2) => `https://docs.oracle.com/en/java/javase/21/docs/api/${g1 || moduleMap[g2.replace(/\//g, '.')]}/${g2}/`);
       };
